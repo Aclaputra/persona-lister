b89dbc352f6240d1c50044dcfbf7832f99ecf883
Aclaputra <=>
2023-06-04 19:18:54 +0700

    initial commit

---
+++ src/main/java/com/aclaputra/persona/web/game/PersonaWebGameApplication.java
@@ -0,0 +1,13 @@
+package com.aclaputra.persona.web.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaWebGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonaWebGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aclaputra/persona/web/game/PersonaWebGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aclaputra.persona.web.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaWebGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
