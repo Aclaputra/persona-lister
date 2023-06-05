1402609b2ce1691f8bb5defb2810277ecece390c
Aclaputra <=>
2023-06-06 03:28:26 +0700

    initial commit

--- src/main/app/src/pages/CharacterList.tsx
+++ src/main/app/src/pages/CharacterList.tsx
@@ -1,8 +1,46 @@
+import {useEffect, useState} from "react";
+// @ts-ignore
+import axios, {AxiosResponse} from 'axios';
 
 export function CharacterList() {
+    interface Characters {
+        id : number;
+        name : string;
+        location : string;
+        image : string;
+    }
+
+    const [characters, setCharacters] = useState<Characters[]>([]);
+
+    const loadCharacters = async (): Promise<any> => {
+        await axios({
+            url: "http://localhost:8081/api/v1/character/list?page=0&size=2",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                // "Authorization": "token"
+            },
+        })
+            .then(data => {
+                console.log(data.data.data.content[0])
+                setCharacters(data.data.data.content)
+                console.log(characters)
+            })
+            .catch(error => console.log(error))
+    }
+
+    useEffect((): void => {
+        loadCharacters().catch(e => console.log(e));
+    }, []);
+
     return (
         <div className="characterlist">
             <h1>Character Lists on Persona 3 FES</h1>
+            <div>
+            {
+                characters.map(item => (<p>{item.name}</p>))
+            }
+            </div>
         </div>
     )
 }
